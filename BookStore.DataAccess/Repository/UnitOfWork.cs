59f024adc13108b592019f0d0a3e637f3ceffa58
matea315 <mateadmatea@gmail.com>
2024-03-13 19:50:01 +0100

    Added repositories and UnitOfWork

---
+++ BookStore.DataAccess/Repository/UnitOfWork.cs
@@ -0,0 +1,26 @@
+﻿using BookStore.DataAccess.Data;
+using BookStore.DataAccess.Repository.IRepository;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.DataAccess.Repository;
+
+public class UnitOfWork : IUnitOfWork
+{
+    private ApplicationDbContext _context;
+    public ICategoryRepository Category {get; private set;}
+    public UnitOfWork(ApplicationDbContext context)
+    {
+        _context = context;
+        Category = new CategoryRepository(_context);
+    }
+
+    public void Save()
+    {
+        _context.SaveChanges();
+    }
+}
